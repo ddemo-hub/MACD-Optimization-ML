22e23d5c645006480b776f09095480ea9dd624f3
dem0 <dem0@example.com>
2023-04-23 12:08:40 +0300

    knn breast cancer

--- project/src/app/k_nearest_neighbor_app.py
+++ project/src/app/k_nearest_neighbor_app.py
@@ -56,3 +56,41 @@ class KNearestNeighborApp():
         Logger.info(f"[K Nearest Neighbor] Test Confusion Matrix: \n{test_confusion_matrix}") 
         
         shutil.copyfile(f"{Globals.project_path}/src/configs/config.yaml", f"{Globals.artifacts_path}/config.yamlignore")
+
+    def breast_cancer(self):
+        from sklearn.datasets import load_breast_cancer
+        
+        dataset = load_breast_cancer()
+        
+        features = dataset.data
+        labels = dataset.target
+        
+        features_train, features_validation, features_test = self.preprocessor.timeseries_split(features) 
+        labels_train, labels_validation, labels_test = self.preprocessor.timeseries_split(labels) 
+        features_train = numpy.concatenate((features_train, features_validation))
+        labels_train = numpy.concatenate((labels_train, labels_validation))
+        
+        if self.config_service.k_nearest_neighnor_oversample == True:
+            from imblearn.over_sampling import RandomOverSampler
+            ros = RandomOverSampler(random_state=42)
+            features_train, labels_train = ros.fit_resample(features_train, labels_train)
+        elif self.config_service.k_nearest_neighnor_undersample == True:
+            from imblearn.under_sampling import RandomUnderSampler
+            rus = RandomUnderSampler(random_state=42)
+            features_train, labels_train = rus.fit_resample(features_train, labels_train)
+        
+        # Initialize Model
+        model = KNearestNeighbor(k=self.config_service.k_nearest_neighnor_k)
+
+        # Train model
+        model.fit(features_train, labels_train)
+        
+        # Test Model
+        predictions = model.predict(features_test)
+        
+        test_f1 = f1_macro(labels_test, numpy.array(predictions))
+        test_confusion_matrix = confusion_matrix(labels_test, numpy.array(predictions))        
+        Logger.info(f"[K Nearest Neighbor] Test F1 Score: {test_f1}") 
+        Logger.info(f"[K Nearest Neighbor] Test Confusion Matrix: \n{test_confusion_matrix}") 
+        
+        shutil.copyfile(f"{Globals.project_path}/src/configs/config.yaml", f"{Globals.artifacts_path}/config.yamlignore")
