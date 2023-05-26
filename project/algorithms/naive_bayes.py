6ac5f84e5a664abc59aa634492f872d161a6954e
dem0-0 <emirkan.derkoken1@gmail.com>
2023-05-26 18:34:59 +0300

    log likelihood implemented, categorical naive bayes bugfix

--- project/algorithms/naive_bayes.py
+++ project/algorithms/naive_bayes.py
@@ -14,21 +14,22 @@ class NaiveBayes():
             self._mean: numpy.ndarray
             self._var: numpy.ndarray
         elif method == "categorical":
-            self.probs: list[list[dict]] = []
+            self._probs: list[list[dict]] = []
 
     def fit(self, X: numpy.ndarray, y: numpy.ndarray):
         n_samples, n_features = X.shape
 
         self._classes = numpy.unique(y)
-        n_classes = len(self._classes)
 
         # Initialize prior 
-        self._prior = numpy.zeros(n_classes, dtype=numpy.float64)
+        self._prior = numpy.zeros(len(self._classes), dtype=numpy.float64)
         
         # If Gaussian Naive Bayes is used, initialize mean and variance
         if self.method == "gaussian":
-            self._mean = numpy.zeros((n_classes, n_features), dtype=numpy.float64)
-            self._var = numpy.zeros((n_classes, n_features), dtype=numpy.float64)
+            self._mean = numpy.zeros((len(self._classes), n_features), dtype=numpy.float64)
+            self._var = numpy.zeros((len(self._classes), n_features), dtype=numpy.float64)
+        elif self.method == "categorical":
+            self._probs = [0] * len(self._classes)
 
         for index, label in enumerate(self._classes):
             X_label = X[y == label]
@@ -49,10 +50,10 @@ class NaiveBayes():
                         len(X_label[:, feature_idx]) 
                         for category in numpy.unique(X_label[:, feature_idx])
                     }
-                    for feature_idx in range(len(X_label))
+                    for feature_idx in range(X_label.shape[1])
                 ]
 
-                self.probs[index] = feature_probs
+                self._probs[index] = feature_probs
 
     def _gauss(self, class_index: int, x: numpy.ndarray):
         mean = self._mean[class_index]
@@ -61,9 +62,9 @@ class NaiveBayes():
         return numpy.exp(-((x - mean) ** 2) / (2 * var)) / numpy.sqrt(2 * numpy.pi * var)
 
     def _predict(self, x: numpy.ndarray):
-        # Calculate the posterior for every class
+        # In order to avoid overflow and underflow problems, log likelihood is used
 
-        if self.method == "gaussian":
+        if self.method == "gaussian": 
             posteriors = [  
                 numpy.log(self._prior[index]) # Prior
                 +
@@ -72,12 +73,15 @@ class NaiveBayes():
             ]
 
         elif self.method == "categorical":
-            posteriors = [
-                self._prior[index] # Prior
-                *
-                numpy.multiply([self.probs[index][feature_index][x[feature_index]] for feature_index in len(x)])    # Posterior
-                for index in range(len(self._classes))
-            ]
+            try:
+                posteriors = [
+                    numpy.log(self._prior[index]) # Prior
+                    +
+                    numpy.sum([numpy.log(self._probs[index][feature_index][x[feature_index]]) for feature_index in range(len(x))])    # Posterior
+                    for index in range(len(self._classes))
+                ]
+            except KeyError:
+                raise Exception(f"One of the categories in the set {x} was not encountered during training")
 
         return self._classes[numpy.argmax(posteriors)]
 
