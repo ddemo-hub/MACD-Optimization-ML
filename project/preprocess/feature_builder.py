17add48494a2851b8d9cce23885ade449e38bd70
dem0-0 <85748316+dem0-0@users.noreply.github.com>
2023-05-28 00:09:26 +0300

    project finalized

--- project/preprocess/feature_builder.py
+++ project/preprocess/feature_builder.py
@@ -1,9 +1,15 @@
+from src.services.config_service import ConfigService
+from src.utils.globals import Globals
+
+from .normalization import rolling_zscore
+
 import pandas 
+import numpy
 
 class FeatureBuilder():
     _must_columns = ["timestamp", "open", "high", "low", "close", "volume"]
 
-    def __init__(self, input_df: pandas.DataFrame, features_path: pathlib.Path, target_features: list):
+    def __init__(self, input_df: pandas.DataFrame, config_service: ConfigService):
         """
         Args:
             input_df (pandas.DataFrame): A dataframe with timestamps and their corresponding OHLCV data
@@ -13,17 +19,44 @@ class FeatureBuilder():
             raise Exception("The input DataFrame must contain the columns [timestamp, open, high, low, close, volume]")
 
         self.candles_df = input_df
-        self.features_path = features_path
-        self.target_features = target_features
-    
+        self.config_service = config_service
+
+        self.features_path = Globals.klines_path.joinpath(config_service.symbol, config_service.interval, "features.csv")
+        self.target_features = config_service.target_features 
+
+        self.categorization = config_service.categorize_features
+
         self._ignore_columns = list(set(self.candles_df.columns) - set(self._must_columns))
+    
+    
+    def normalize_feature(self, column: pandas.Series):
+        if column.name[-1] != "0":
+            column = rolling_zscore(
+                column=column, 
+                window=self.config_service.standardization_window, 
+                limit=self.config_service.standardization_limit
+            )
+        
+        if column.name[-2] != "0":
+            column = column / column.shift(1)
+
+        return column
+    
+    
+    def _categorize_features(self):
+        ...
+    
     
     def build_all():
         ...
         
-    # Features are to be kept confidential
+    # Features will be kept confidential
     def read_features(self):
         features_df = pandas.read_csv(self.features_path)
         
         self.candles_df = self.candles_df.merge(features_df, on="timestamp")
+
+        if self.categorization == True:
+            self._categorize_features()
+        
         self.candles_df = self.candles_df[["timestamp"] + self.target_features + self._ignore_columns]
